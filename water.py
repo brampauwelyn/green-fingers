99a01d31dc64f78f7aa468966b3b9b4197f1bee8
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 22:23:08 +0200

    progress on script

--- water.py
+++ water.py
@@ -1,27 +1,42 @@
 from time import sleep
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-button1 = 3
 relay1 = 17
-GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(relay1, GPIO.OUT)
+
+# Define Colors for console output
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Function to Reset GPIOs
+def resetGPIO(relay):
+    GPIO.output(relay1,False)
+    GPIO.cleanup()
 
 # 27 sec --> 200ml
 # 13.5 sec --> 100ml
 
 # Reset GPIO's
 # Disable Relay by default
-GPIO.cleanup()
-GPIO.output(relay1,False)
+resetGPIO(relay1, False)
+
 while(1):
-    print "Watering Plants"
-    GPIO.output(relay1,True)
+    print(colors.BLUE + "Watering Plants...")
+    # GPIO.output(relay1,True)
     #Should give 100 ml water
     sleep(13.5)
-    GPIO.output(relay1,False)
-    print "Stop Watering Plants"
+    # Disable Relay
+    resetGPIO(relay1, False)
+    print (color.GREEN + "Done Watering Plants")
     break
 # clean up GPIO
-GPIO.cleanup()
+# GPIO.cleanup()
 # Exit script
 exit()
