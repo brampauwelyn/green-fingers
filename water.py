372665fddde82390762141b425b80f623a47ffd7
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 17:15:44 +0200

    progress on script

--- water.py
+++ water.py
@@ -4,9 +4,9 @@ GPIO.setmode(GPIO.BCM)
 button1 = 3
 relay1 = 2
 GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(button1, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(relay1, GPIO.OUT)
 while(1):
     if GPIO.input(button1) == 0:
         print "Button 1 was pressed"
-        GPIO.output(relay1) == 1
-        sleep(.1)
+        GPIO.output(relay1,True)
+        sleep(.5)
