0f081612dee16b10c746fb2f40e6e4f8423b1d8a
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 16:52:11 +0200

    progress on script

--- water.py
+++ water.py
@@ -8,7 +8,7 @@ GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(2, GPIO.OUT, )
 
 while True:
-    if (GPIO.input(3) ==1):
+    if (GPIO.input(3) == 0):
         print ("button pressed")
 
 GPIO.cleanup()
