f70a7818ddf345e50f43da8a9920c2063b4313aa
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 16:54:31 +0200

    progress on script

--- water.py
+++ water.py
@@ -1,14 +1,9 @@
 import RPi.GPIO as GPIO
-import time as t
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-GPIO.setup(2, GPIO.OUT, )
+GPIO.setup(3, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 while True:
-    if (GPIO.input(3) == 0):
-        print ("button pressed")
-
-GPIO.cleanup()
+    if(GPIO.input(3) == 1):
+        print(“Button 1 pressed”)
