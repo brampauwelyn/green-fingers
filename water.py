44232b0e18523f83f9efb3014b86ad28963fb2aa
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 17:09:27 +0200

    progress on script

--- water.py
+++ water.py
@@ -1,7 +1,9 @@
- from gpiozero import Button
- button = Button(3)
-
- button.wait_for_press()
- print('You pushed me')
-
- 
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+button1 = 3
+GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+while(1):
+    if GPIO.input(button1) == 0:
+        print "Button 1 was pressed"
+        sleep(.1)
