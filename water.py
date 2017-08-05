6366d53198ceba1706d4a3c6da8b1a03c1ffef40
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 17:12:33 +0200

    progress on script

--- water.py
+++ water.py
@@ -2,8 +2,11 @@ from time import sleep
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 button1 = 3
+relay1 = 2
 GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(button1, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
 while(1):
     if GPIO.input(button1) == 0:
         print "Button 1 was pressed"
+        GPIO.output(relay1) == 1
         sleep(.1)
