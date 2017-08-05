ac45d1668b3b0aaad9f2e597c7cd8bb9ae9f0862
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 17:50:04 +0200

    progress on script

--- water.py
+++ water.py
@@ -2,7 +2,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 button1 = 3
-relay1 = 2
+relay1 = 17
 GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(relay1, GPIO.OUT)
 while(1):
