c65688cb033fa02becbf68076e6a58183603d9bb
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 16:57:32 +0200

    progress on script

--- water.py
+++ water.py
@@ -3,8 +3,6 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-while true:
+while True:
     if(GPIO.input(3) == 1):
         print("Yeah")
-
-        
