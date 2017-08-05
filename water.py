dbee75243c8636477fcdf5648117cdc1e34cacad
Bram Pauwelyn <brampauw@gmail.com>
2017-08-05 21:23:39 +0200

    progress on script

--- water.py
+++ water.py
@@ -9,12 +9,18 @@ GPIO.setup(relay1, GPIO.OUT)
 # 27 sec --> 200ml
 # 13.5 sec --> 100ml
 
+# Reset GPIO's
+# Disable Relay by default
+GPIO.cleanup()
+GPIO.output(relay1,False)
 while(1):
     print "Watering Plants"
     GPIO.output(relay1,True)
     #Should give 100 ml water
     sleep(13.5)
-    False
+    GPIO.output(relay1,False)
+    print "Stop Watering Plants"
+    break
 # clean up GPIO
 GPIO.cleanup()
 # Exit script
